742713de0cebb3ea2b199368d534a533f744ac91
Josemar <josemar.rincon@gmail.com>
2020-03-28 10:11:25 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+
+import Routes from './src/routes';
+
+
+
+// integrar com api instalar axios
+// npm install axios
+
+export default function App() {
+  return (
+   <Routes/>
+  );
+}
+
+
